af9d16bf0af959bc3c6201ed0eecccc780e79f41
Mokhtar <mokht@rmi.al>
2023-05-10 04:38:39 +0200

    feat: bump version in package.json without publishing npm package (#5)

--- release.config.js
+++ release.config.js
@@ -3,6 +3,12 @@ module.exports = {
   'plugins': [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false
+	  },
+    ],
     '@semantic-release/github',
     [
       '@semantic-release/changelog',
